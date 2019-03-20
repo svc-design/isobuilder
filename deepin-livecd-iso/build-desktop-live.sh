eaae3bc48e4c28926d0158cd6702fb6a7c524b43
shenlan <xz@onwalk.net>
2019-03-20 07:31:50 +0000

    Custom LiveCD: release for deepin 15.9.2

--- deepin-livecd-iso/build-desktop-live.sh
+++ deepin-livecd-iso/build-desktop-live.sh
@@ -3,17 +3,15 @@
 set -x
 
 export REPO_LOCAL=file:///home/shenlan/workspaces/lion-repo-core/
-export REPO_NET=http://mirrors.ustc.edu.cn/debian/
-export DIST=stable
+export REPO_NET=http://mirrors.tuna.tsinghua.edu.cn/deepin/
+export DIST=lion
 export CHROOT=$HOME/chroot
-export PKG_CORE="locales,busybox,systemd,systemd-sysv,initramfs-tools,ssh,tar,iptables,linux-image-amd64,grub-efi,live-boot,vim,isc-dhcp-client,iproute2"
+export PKG_CORE="locales,busybox,systemd,systemd-sysv,initramfs-tools,linux-image-amd64,grub-efi,live-boot"
 
 function do_prep()
 {
     rm -rvf  $HOME/chroot/
     rm -rvf  $HOME/LIVE_BOOT/
-    mkdir -p $HOME/chroot
-    mkdir -p $HOME/LIVE_BOOT/{scratch,image/live}
 #    apt install debootstrap squashfs-tools xorriso grub-pc-bin grub-efi-amd64-bin mtools -y
 }
 
@@ -30,17 +28,18 @@ mount -t proc proc   $CHROOT/proc
 mount -t sysfs sysfs $CHROOT/sys
 
 cat <<'EOF' >${CHROOT}/etc/apt/sources.list
-deb [trusted=yes] http://mirrors.ustc.edu.cn/debian/ stable main contrib non-free
 deb [trusted=yes] http://mirrors.tuna.tsinghua.edu.cn/deepin/ lion main contrib non-free
 EOF
 
 cat <<'EOF' >${CHROOT}/tmp/set-livecd.sh
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
 useradd live
 groupadd live
 mkdir /home/live && chown live:live /home/live/ 
 echo "root:live" | chpasswd 
 echo "live:live" | chpasswd 
+apt update && apt install ssh tar iptables vim isc-dhcp-client iproute2 deepin-keyring -y
 EOF
 chmod 755 $HOME/chroot/tmp/set-livecd.sh
 chroot $CHROOT "/tmp/set-livecd.sh" 
@@ -50,12 +49,18 @@ chroot $CHROOT "/tmp/set-livecd.sh"
 function do_second_stage()
 {
 
-cat <<'EOF' >>${CHROOT}/tmp/set-livecd.sh
+cat <<'EOF' >${CHROOT}/etc/apt/sources.list
+deb [trusted=yes] http://mirrors.tuna.tsinghua.edu.cn/deepin/ lion main contrib non-free
+EOF
+
+cat <<'EOF' >${CHROOT}/tmp/set-livecd.sh
+#!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt update && apt -f install -y
-apt install live-config linux-image-4.19.0-2-amd64 linux-tools linux-headers-4.19.0-2-all linux-source-4.19 firmware-misc-nonfree firmware-linux-free firmware-iwlwifi bluez-firmware network-manager lightdm fcitx fcitx-frontend-qt5 fcitx-frontend-gtk3 sogoupinyin xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xserver-xorg-input-wacom xinit -y
+apt install live-config linux-image-deepin-amd64 linux-tools-4.15.0-30deepin linux-headers-4.15.0-30deepin linux-source-4.15.0 firmware-misc-nonfree firmware-linux-free firmware-iwlwifi bluez-firmware network-manager lightdm fcitx fcitx-frontend-qt5 fcitx-frontend-gtk3 sogoupinyin xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xserver-xorg-input-wacom xinit -y
 apt install --no-install-recommends dde deepin-installer deepin-terminal google-chrome-stable -y
-apt-get remove xterm --purge -y 
+apt-get remove linux-image-amd64 linux-image-4.9.0-8-amd64  xterm --purge -y 
+update-initramfs -u
 #apt install kubeadm kubectl kubelet docker.io ansible teamviewer dingtalk touchpad-indicator deepin-screenshot wps-office thunderbird thunderbird-locale-zh-hans -y
 EOF
 chmod 755 $HOME/chroot/tmp/set-livecd.sh
@@ -77,6 +82,8 @@ rm -rvf $CHROOT/var/cache/apt/archives/
 function do_mk_file()
 {
 
+mkdir -p $HOME/chroot
+mkdir -p $HOME/LIVE_BOOT/{scratch,image/live}
 mksquashfs $HOME/chroot $HOME/LIVE_BOOT/image/live/filesystem.squashfs -comp xz 
 cp $HOME/chroot/boot/vmlinuz-* $HOME/LIVE_BOOT/image/vmlinuz
 cp $HOME/chroot/boot/initrd.img-* $HOME/LIVE_BOOT/image/initrd
@@ -91,6 +98,10 @@ set default="0"
 set timeout=30
 
 menuentry "Desktop Live" {
+    linux /vmlinuz boot=live quiet
+    initrd /initrd
+}
+menuentry "Desktop Live overlay" {
     linux /vmlinuz boot=live union=overlay quiet
     initrd /initrd
 }
@@ -118,13 +129,13 @@ xorriso -as mkisofs \
     -no-emul-boot \
     -append_partition 2 0xef ${HOME}/LIVE_BOOT/scratch/efiboot.img \
     -graft-points "${HOME}/LIVE_BOOT/image" /EFI/efiboot.img=$HOME/LIVE_BOOT/scratch/efiboot.img \
-    -output "${HOME}/LIVE_BOOT/deepin-desktop-core-20181210.iso"
+    -output "${HOME}/LIVE_BOOT/Deepin-15.9.2-custom-LiveCD.iso"
 }
 
-#do_prep
+do_prep
 do_first_stage
-#do_second_stage
+do_second_stage
 do_umount
-#do_clean
+do_clean
 do_mk_file
 do_mkiso
